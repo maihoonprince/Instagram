968519bb6646f30ef71ce12955f355f8cf3a30b8
Prince <maihoonprinx@gmail.com>
2024-03-27 16:19:50 +0530

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import Home from './components/Home';
 import Navbar from './components/navbar/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from './components/profile/Profile';
+import Signin from './components/signin/Signin';
+import Signup from './components/signup/Signup';
 
 function App() {
   return (
+    <BrowserRouter>
+
     <div className="App">
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/signup' element={<Signup/>}></Route>
+        <Route path='/signin' element={<Signin/>}></Route>
+        <Route path='/profile' element={<Profile/>}></Route>
+      </Routes>
     </div>
+ 
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,19 +1,32 @@
 import React from 'react';
-
-import logo from '../../Asset/logo.png';
 import "./Navbar.css";
+import logo from '../../Asset/logo.png';
+
+import { Link } from "react-router-dom";
+
 
 const Navbar = () => {
-  return (
-    <div className='Navbar'>
-        <img src={logo} alt='Logo' className='logo'/>
-        <ul>
-            <li>Signin</li>
-            <li>Signup</li>
-            <li>Profile</li>
-        </ul>
-    </div>
-  )
+    return (
+        <div className='Navbar'>
+            <img src={logo} alt='Logo' className='logo' />
+            <ul>
+                {/* <Link to="/"></Link> */}
+
+                <Link to="/Signup">
+                    <li>SignUp</li>
+                </Link>
+
+                <Link to="/Signin">
+                    <li>SignIn</li>
+                </Link>
+
+                <Link to="/Profile">
+                    <li>Profile</li>
+                </Link>
+
+            </ul>
+        </div>
+    )
 }
 
 export default Navbar
