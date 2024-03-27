d3c17dc123d0dc5365b9d4572562e21312e9c3e5
Prince <maihoonprinx@gmail.com>
2024-03-27 19:28:37 +0530

    completed sign in page frontend

--- frontend/src/components/signin/Signin.js
+++ frontend/src/components/signin/Signin.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import logo from "../../Asset/logo.png";
+import {Link} from "react-router-dom";
+import "./signin.css"
 
 const Signin = () => {
   return (
-    <div>Signin</div>
+    <div className='signin'>
+      <div>
+        <div className="login-form">
+          <img id='signin-logo' src={logo} alt="Logo" />
+          <div>
+            <input type="email" name='email' id='email' placeholder='Email' />
+          </div>
+
+          <div>
+            <input type="password" id='password' name='password' placeholder='Password' />
+          </div>
+
+          <input type="submit" id='login-btn' value="Sign In" />
+        </div>
+
+        <div className="login-form2">
+        Don't have an Account ?
+          <Link to="/signup">
+          <span style={{color:"blue", cursor:"pointer"}}> Sign Up</span>
+          </Link>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
